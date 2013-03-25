502e19dce1d60c9661829aa1fbd3f56bb6c3630a
hagino3000 <hagino.3000@gmail.com>
2013-03-25 13:54:30 +0900

    Add setup script

---
+++ create_link.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dev/dotfiles/.zshrc.osx ~/.zshrc.osx
+ln -s ~/dev/dotfiles/.zshrc.perl ~/.zshrc.perl
+ln -s ~/dev/dotfiles/.zshrc.aws ~/.zshrc.aws
+ln -s ~/dev/dotfiles/.zshrc.ubuntu ~/.zshrc.ubuntu
+ln -s ~/dev/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dev/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dev/dotfiles/.hgrc ~/.hgrc
+ln -s ~/dev/dotfiles/.hgignore_global ~/.hgignore_global
+ln -s ~/dev/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dev/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dev/dotfiles/.screenrc ~/.screenrc
+
