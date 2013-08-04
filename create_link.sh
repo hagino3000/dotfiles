96690021d6a41faf90c6ac70635dbc6475cedf33
Takashi Nishibayashi <takashi_nishibayashi@voyagegroup.com>
2013-08-04 11:51:58 +0900

    Mod create_shim.sh

--- create_link.sh
+++ create_link.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+
 ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dev/dotfiles/.zshrc.osx ~/.zshrc.osx
+os=`uname`
+
+if [ $os = "Darwin" ]; then
+    ln -s ~/dev/dotfiles/.zshrc.osx ~/.zshrc.osx
+fi
+
+if [ $os = "Linux" ]; then
+    ln -s ~/dev/dotfiles/.zshrc.ubuntu ~/.zshrc.ubuntu
+fi
+
 ln -s ~/dev/dotfiles/.zshrc.perl ~/.zshrc.perl
-ln -s ~/dev/dotfiles/.zshrc.aws ~/.zshrc.aws
-ln -s ~/dev/dotfiles/.zshrc.ubuntu ~/.zshrc.ubuntu
 ln -s ~/dev/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dev/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dev/dotfiles/.vim ~/.vim
 ln -s ~/dev/dotfiles/.hgrc ~/.hgrc
 ln -s ~/dev/dotfiles/.hgignore_global ~/.hgignore_global
 ln -s ~/dev/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dev/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dev/dotfiles/.screenrc ~/.screenrc
-ln -s ~/dev/dotfiles/.vim ~/.vim
+ln -s ~/dev/dotfiles/.tmux.conf ~/.tmux.conf
 
