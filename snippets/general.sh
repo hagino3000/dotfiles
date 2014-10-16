50ecfda1b081d41358c420bf118a5bdc85aaeadd
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-10-16 13:25:34 +0900

    Add snipets

--- snippets/general.sh
+++ snippets/general.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo 'Dont run this script!!'
+exit 255
 
 # Check crontab
 diff -u <(crontab -l) crontab.ini
 
 # Update crontab
-# crontab ceontab.ini
+crontab ceontab.ini
+
+# Use yesterday to command arg
+./bin/python -m report.main --date `date +"%Y-%m-%d" --date '1 days ago'`
