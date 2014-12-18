f5c4ba977fbab30d56ee4e7e078a943823ad77a2
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-12-18 12:45:30 +0900

    Add sunippets

--- snippets/general.sh
+++ snippets/general.sh
@@ -10,3 +10,7 @@ crontab ceontab.ini
 
 # Use yesterday to command arg
 ./bin/python -m report.main --date `date +"%Y-%m-%d" --date '1 days ago'`
+
+# Redirect
+command >> file 2>&1
+command 1>file 2>&1
