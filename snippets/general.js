609b23aac0758fbb2dfdb9aaededf69cd4b0e33d
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-01-14 15:38:28 +0900

    Add js snippet

---
+++ snippets/general.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function urlArgs() {
+    var args = {};
+
+    var query = ocation.search.substring(1);
+    var pairs = query.split("&");
+    for (var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf('=');
+        if (pos == -1) continue;
+        var name = pairs[i].substring(0, pos);
+        var value = pairs[i].substring(pos + 1);
+        value = decodeURIComponent(value);
+        args[name] = value;
+    }
+    return args;
+}
