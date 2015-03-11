9b619901ecfba0dc5adf9ba92a46ae415ecc4a45
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-03-11 10:46:55 +0900

    Add snippets

--- snippets/general.sh
+++ snippets/general.sh
@@ -5,6 +5,21 @@ exit 255
 # move current file directory
 cd `dirname $0`
 
+script_dir_path=$(cd $(dirname $0) && pwd)
+cd $script_dir_path
+
+# See http://qiita.com/m-yamashita/items/889c116b92dc0bf4ea7d?utm_source=Qiita%E3%83%8B%E3%83%A5%E3%83%BC%E3%82%B9&utm_campaign=a349fbe9d6-Qiita_newsletter_146_03_11_2015&utm_medium=email&utm_term=0_e44feaa081-a349fbe9d6-10307105
+# シンボリックリンクの中にいる場合
+case "${OSTYPE}" in
+  freebsd*|darwin*)
+    scriptDirPath=$(dirname $(greadlink -f $0))
+    ;;
+  linux*)
+    scriptDirPath=$(dirname $(readlink -f $0))
+    ;;
+esac
+
+
 # Check file size each directories
 du -d 1
 
