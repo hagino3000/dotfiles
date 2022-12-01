bf89e192263849e6a20528632012d0a6eb1df7f5
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2022-12-01 12:51:07 +0900

    add snippets

--- snippets/shell.sh
+++ snippets/shell.sh
@@ -36,20 +36,45 @@ crontab ceontab.ini
 command >> file 2>&1
 command 1>file 2>&1
 
-# loop
+# Check EC2 instance meta data
+curl http://169.254.169.254/latest/meta-data/
+
+###########################################################
+# function
+###########################################################
+hoge () {
+    TABLE=$1
+    echo --------------------------------------
+    echo Check $TABLE
+    echo --------------------------------------
+}
+
+hoge table_1
+hoge table_2
+
+
+###########################################################
+# For loop
+###########################################################
+# with list
+fuga_types=(aaa bbb ccc)
+for fuga_type in ${fuga_types[@]}
+do
+  echo Start $fuga_type
+  # do something
+done
+
+# with range
 for i in {00..23};do
     echo $i
 done
 
-# 日付ループして何かやる
+# with date range
 START_DATE="20171010"
 END_DATE="20171023"
 
 for (( DATE=${START_DATE} ; ${DATE} < ${END_DATE} ; DATE=`date -d "${DATE} 1 day" '+%Y%m%d'` )) ; do
     DF=`date -d ${DATE} '+%Y-%m-%d'`
     D=`date -d ${DATE} '+%Y%m%d'`
-    echo make run_hoge ENV=prod LUIGI_CONFIG_PATH=../conf/luigi.local.cfg TARGET_DATE=${D}
+    echo make run_hoge TARGET_DATE=${D}
 done
-
-# Check EC2 instance meta data
-curl http://169.254.169.254/latest/meta-data/
