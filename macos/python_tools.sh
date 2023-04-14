39a1f409a7bbe981b8f50a2227c03180d3c2cd6d
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2023-04-14 11:15:52 +0900

    Remove percol

--- macos/python_tools.sh
+++ macos/python_tools.sh
@@ -8,6 +8,5 @@ pip3 install virtualenv --user
 pip3 install ipython --user
 pip3 install httpie --user
 pip3 install requests --user
-pip3 install percol --user
 pip3 install awslogs --user
 pip3 install awscli --user
