5eca0b80530b11a4b54a26b0fae065a34782477b
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2026-08-13 20:07:50 +0900

    Add skk stuffs

--- macos/setup_skk.sh
+++ macos/setup_skk.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd ./dictionaries
+mkdir -p dict_work
+cd ./dict_work
 curl -O https://skk-dev.github.io/dict/SKK-JISYO.L.gz
 curl -O https://raw.githubusercontent.com/ymrl/SKK-JISYO.emoji-ja/master/SKK-JISYO.emoji-ja.utf8
 curl -O https://raw.githubusercontent.com/uasi/skk-emoji-jisyo/refs/heads/master/SKK-JISYO.emoji.utf8
 cp ./* ~/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries
+
+cd ~/dev/dotfiles/yaskkserv2
+cargo build --release;
+mkdir -p ~/.yaskkserv2/bin
+mv ~/dev/dotfiles/yaskkserv2/target/release/yaskkserv2 ~/.yaskkserv2/bin
+mv ~/dev/dotfiles/yaskkserv2/target/release/yaskkserv2_make_dictionary ~/.yaskkserv2/bin
